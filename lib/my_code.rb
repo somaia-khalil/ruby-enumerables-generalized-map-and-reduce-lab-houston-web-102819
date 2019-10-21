66d282db0884ec0905c263e0a1d369914146453d
somaia khalil <sumaya_khair@live.com>
2019-10-21 02:19:48 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,31 @@
-# Your Code Here
+def map (array)
+  new = [ ]
+  counter = 0 
+  while counter < array.size 
+  new.push(yield(array[counter]))
+  counter+=1
+  end
+  return new
+end
+
+
+def reduce (array , startpoint = nil)
+  
+  if startpoint
+  counter=0 
+  total = startpoint
+else 
+  counter = 1
+  total = array[0]
+end
+  
+  while counter < array.size 
+  total =  yield(total, array[counter])
+  counter+=1
+  end
+  
+  return total
+  
+end
+
+
